ea575db3fa76fa1c22c83b864ee9e107719b4a70
Bogna Lew <100587236+BognaLew@users.noreply.github.com>
2022-04-17 15:40:05 +0200

    Delete Projects directory

--- Projects/Project_1/Grass.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Plant.h"
-
-class Grass : public Plant {
-public:
-	Grass(int whenWasBorn, Coordinates coords, World* world);
-	void GetOrganismName() override;
-	~Grass();
-};
--- Projects/Project_1/Wolf.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Animal.h"
-
-class Wolf : public Animal {
-public:
-	Wolf(int  whenWasBorn, Coordinates coords, World* world);
-	void GetOrganismName() override;
-	~Wolf();
-};
